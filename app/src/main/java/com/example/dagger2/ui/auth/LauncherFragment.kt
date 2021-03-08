d2b05719589d53a6d07bea9b21ae59f08adca68f
Sobir1999 <mustafoyevsobir0898@gmail.com>
2021-03-08 20:35:33 +0500

    Initial commit

---
+++ app/src/main/java/com/example/dagger2/ui/auth/LauncherFragment.kt
@@ -0,0 +1,48 @@
+package com.example.dagger2.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import com.example.dagger2.R
+
+class LauncherFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_launcher, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.register).setOnClickListener {
+            navRegister()
+        }
+
+        view.findViewById<TextView>(R.id.login).setOnClickListener {
+            navLogin()
+        }
+
+        view.findViewById<TextView>(R.id.forgot_password).setOnClickListener {
+            navForgotPassword()
+        }
+    }
+
+    private fun navForgotPassword() {
+        findNavController().navigate(R.id.action_launcherFragment_to_forgotPasswordFragment)
+    }
+
+    private fun navLogin() {
+        findNavController().navigate(R.id.action_launcherFragment_to_loginFragment)
+    }
+
+    private fun navRegister() {
+        findNavController().navigate(R.id.action_launcherFragment_to_registerFragment)
+    }
+}
